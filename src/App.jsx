45e7d311d5edce97e1b06df733926b85ff2f53d5
Haris Bin Waseem <hariswaseem430@gmail.com>
2023-09-17 20:53:14 +0500

    Add Lazy Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import "./App.css";
 import { Routes, Route } from "react-router";
-import TodosList from "./components/Todos/TodosList";
+import { Suspense, lazy } from "react";
 import Navbar from "./components/Navbar";
-import UserDetails from "./components/UserDetails";
+
+const TodosList = lazy(() => import("./components/Todos/TodosList"));
+const UserDetails = lazy(() => import("./components/UserDetails"));
+// const Navbar = lazy(() => import("./components/Navbar"));
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Navbar />} />
-        <Route path="/user-details" element={<UserDetails />} />
-        <Route path="/todos-list" element={<TodosList />} />
-      </Routes>
+      <Suspense fallback={<h1>Loading..............</h1>}>
+        <Routes>
+          <Route path="/" element={<Navbar />} />
+          <Route path="/user-details" element={<UserDetails />} />
+          <Route path="/todos-list" element={<TodosList />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
--- src/Services/api/TodosAPI.jsx
+++ src/Services/api/TodosAPI.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const TodosAPI = createApi({
   reducerPath: "TodosAPI",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3002/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/" }),
   tagTypes: ["Todos"],
   endpoints: (builder) => ({
     getAllTodos: builder.query({
